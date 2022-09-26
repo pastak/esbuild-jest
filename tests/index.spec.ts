ae05623b0dab52a88cf94988797a6bfec24a5a57
pastak <pasta0915@gmail.com>
2022-09-26 19:51:26 +0900

    cherry picking aelbore/esbuild-jest/pull/50
    https: //github.com/aelbore/esbuild-jest/pull/50
    Co-Authored-By: Björn Brauer <zaubernerd@zaubernerd.de>
    Co-Authored-By: Markus Wolf <327445+KnisterPeter@users.noreply.github.com>
    Co-Authored-By: Robert Kowalski <rok@kowalski.gd>

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import fs from 'fs'
 import path from 'path'
 
@@ -19,8 +22,8 @@ const process = (sourcePath: string, options?: Options) => {
     ...(options || {})
   })
 
-  const config = { ...defaults, cwd: path.resolve() } as any
-  const output = Transformer.process(content, sourcePath, config) as { code: string, map: string }
+  const opts = { config: { ...defaults, cwd: path.resolve() } } as any
+  const output = Transformer.process(content, sourcePath, opts) as { code: string, map: string }
 
   return { ...output }
 }
